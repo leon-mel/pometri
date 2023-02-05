6e86a98266540c5b915e442135b6d021a5de67a7
leon-mel <leontosherii@yahoo.de>
2023-02-05 12:57:25 +0100

    configurated firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:speeddatingapp/UI/Login/loginMain.dart';
 import 'package:speeddatingapp/domain/entities/classSizeConfig.dart';
@@ -6,7 +7,9 @@ import 'package:speeddatingapp/navigator/profileScreen.dart';
 import 'domain/entities/classRoutes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const SpeedDatingApp());
 }
 
